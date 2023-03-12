cf8bb6701b559989aaec67b7cfc9e1c26d77e4ac
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2023-03-12 11:46:02 +0200

    [feature] Logo update

--- partner-up-ui/src/app/page.tsx
+++ partner-up-ui/src/app/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
+function PartnerUpLogo() {
+    return (
+        <a href="#" className="font-bold p-3 border border-green-400 hover:bg-gray-50 dark:hover:bg-neutral-800">
+            <span className="dark:text-white">partner</span>
+            <span className="text-green-400">UP</span>
+        </a>
+    );
+}
+
 export default function Home() {
     return (
         <main className="bg-white dark:bg-neutral-900 w-full h-screen flex justify-center items-center">
-            <div className="font-bold p-3 border border-green-400">
-                <span className="dark:text-white">partner</span>
-                <span className="text-green-400">UP</span>
-            </div>
+            <PartnerUpLogo></PartnerUpLogo>
         </main>
     );
 }
