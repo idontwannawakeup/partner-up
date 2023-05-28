f96af01e1118e49dea9ab7ed67915467248bb255
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2023-05-28 18:59:06 +0300

    [feature] People service migration

---
+++ partner-up-server/src/Microservices/Identity/People/PartnerUp.Identity.People.API/Common/Mapping/UserEventsMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PartnerUp.EventBus.Messages;
+using PartnerUp.Identity.People.BusinessLogic.Common.Models.Requests;
+using PartnerUp.Identity.People.BusinessLogic.Common.Models.Responses;
+
+namespace PartnerUp.Identity.People.API.Common.Mapping;
+
+public class UserEventsMapping : Profile
+{
+    public UserEventsMapping()
+    {
+        CreateMap<UserSignUpRequest, UserCreatedEvent>();
+        CreateMap<UserResponse, UserChangedEvent>();
+    }
+}
---
+++ partner-up-server/src/Microservices/Identity/People/PartnerUp.Identity.People.BusinessLogic/Interfaces/Services/IUsersService.cs
@@ -0,0 +1,16 @@
+﻿using PartnerUp.Identity.People.BusinessLogic.Common.Models.Requests;
+using PartnerUp.Identity.People.BusinessLogic.Common.Models.Responses;
+using PartnerUp.Identity.Persistence.People.Common.Parameters;
+using PartnerUp.Shared.Pagination;
+
+namespace PartnerUp.Identity.People.BusinessLogic.Interfaces.Services;
+
+public interface IUsersService
+{
+    Task<IEnumerable<UserResponse>> GetAsync();
+    Task<PagedList<UserResponse>> GetAsync(UsersParameters parameters);
+    Task<UserResponse> GetByIdAsync(Guid id);
+    Task<UserResponse> UpdateAsync(UserRequest request);
+    Task<string> SetAvatarForUserAsync(UserAvatarRequest request);
+    Task DeleteAsync(Guid id);
+}
